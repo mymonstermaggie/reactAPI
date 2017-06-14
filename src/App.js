f8bd5f9017059d2cd9dc2a890e2243ba755f3caf
mymonstermaggie <maggie@remote-com.com>
2017-06-13 20:56:00 -0500

    in class get nasa data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: '',
+      img: ''
+    }
+  };
+  componentWillMount() {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=2GqR3qygi0bX12jeIpTa8k4Fwn9zwXz9bM72c5oT')
+      .then(res => {
+        console.log(res);
+        this.setState({results: res.data.title});
+        this.setState({img: res.data.url});
+      })
+      .catch(err => {
+        console.log(err);
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={this.state.img} className="App-logo" alt="logo" />
+          <h2>{this.state.results}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div id="test"></div>
       </div>
     );
   }
